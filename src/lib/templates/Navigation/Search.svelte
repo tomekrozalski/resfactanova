78861bf9b56b3d3db2cff5974bd85eee331a987f
Tomek Rozalski <github@tomaszrozalski.pl>
2023-11-17 20:45:04 +0100

    Minor a18y fixes to search section

--- src/lib/templates/Navigation/Search.svelte
+++ src/lib/templates/Navigation/Search.svelte
@@ -9,14 +9,24 @@
 		<input
 			id="search"
 			type="text"
-			class="border-gray-800 focus:border-gray-600 peer w-full rounded border px-2 py-1 focus:outline-none dark:border-black"
+			class="border-gray-800 focus:border-gray-600 peer w-full rounded border px-2 py-1 text-sm focus:outline-none dark:border-black dark:p-3"
 		/>
 		<button
 			aria-label="Rozpocznij wyszukiwanie"
 			type="submit"
-			class="text-gray-400 absolute right-0 top-1/2 -translate-y-1/2 p-2 pr-3 peer-focus:text-black"
+			class="text-gray-400 absolute bottom-0 right-0 top-0 px-3 peer-focus:text-black dark:px-3"
 		>
 			<Fa icon={faMagnifyingGlass} />
 		</button>
 	</div>
 </form>
+
+<style>
+	:global(.enlarge) input {
+		padding: theme('spacing.3');
+	}
+
+	:global(.enlarge) button {
+		padding-inline: theme('spacing.4');
+	}
+</style>
