e5f1fe9fdb70223026a96f7f2c8216223fb8f3b3
Tomek Rozalski <tomek.rozalski@decathlon.com>
2023-11-25 21:56:12 +0100

    Search works. Need to work on UI

--- src/routes/szukaj/[phrase]/+page.server.ts
+++ src/routes/szukaj/[phrase]/+page.server.ts
@@ -1,5 +1,7 @@
 import { error } from '@sveltejs/kit';
 import contentfulFetch from '$lib/db/contentful-fetch';
+import formatArticle from './utils/formatArticle';
+import type { FormattedArticleTypes } from './utils/Article.d';
 
 export const load = async ({ params }) => {
 	const phrase = params.phrase;
@@ -8,11 +10,42 @@ export const load = async ({ params }) => {
 		throw error(404, 'Incorrect param');
 	}
 
-	const searchResponse = await contentfulFetch(/* GraphQL */);
+	const searchResponse = await contentfulFetch(`
+		{
+			articleCollection (where: { OR: [
+				{ title_contains: "${phrase}" },
+				{ abstract_contains: "${phrase}" },
+				{ authors: { fullName_contains: "${phrase}" }}
+			]}) {
+				items {
+					abstract
+					authorsCollection(limit: 10) {
+						items {
+							fullName
+							slug
+						}
+					}
+					pdf {
+						url
+					}
+					title
+				}
+			}
+		}
+	`);
 
 	if (!searchResponse.ok) {
 		throw error(404, { message: 'Searching in Contentful database failed' });
 	}
 
-	return { test: true };
+	const articlesData = await searchResponse.json();
+	const articles = articlesData?.data?.articleCollection?.items;
+
+	if (!articles?.length) {
+		return { articles: [] };
+	}
+
+	const formattedArticles: FormattedArticleTypes[] = articles.map(formatArticle);
+
+	return { articles: formattedArticles };
 };
--- src/routes/szukaj/[phrase]/+page.svelte
+++ src/routes/szukaj/[phrase]/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	export let data;
-
-	$: console.log(data);
 </script>
+
+<ul>
+	{#each data.articles as { abstract, authors, pdf, title }}
+		<li class="list-inside list-disc">
+			{#if authors?.length}
+				{#each authors as { fullName, slug }, index}
+					<a href="/autor/{slug}">{fullName}</a
+					>{#if authors.length > 1 && index + 1 !== authors.length}
+						,{' '}
+					{/if}
+				{/each}
+			{/if}{title}
+		</li>
+	{/each}
+</ul>
